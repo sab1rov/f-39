0420bffd0e24c34225941f32bb2fef86209847b1
nosirxon650 <nurmetovnosirxon668@gmail.com>
2025-01-09 15:49:54 +0500

    bug ficed

--- src/components/Staff.jsx
+++ src/components/Staff.jsx
@@ -1,29 +1,26 @@
-export const Staff = ({ imgUrl, title, price, price2, price3, button }) => {
+export const Staff = ({ imgUrl, title, price, old_price }) => {
     return (
-        <div className="staff-list">
-            <img className="staff-img" src={imgUrl} alt="" />
-            <h4 className="h4-title">{title}</h4>
-            <div className="cost">
+        <div className='staff-list'>
+            <img className='staff-img' src={imgUrl} alt='' />
+            <h4 className='h4-title'>{title}</h4>
+            <div className='cost'>
                 <div>
-                    <p>Розница:</p>
+                    <p>Price:</p>
                     <b>{price}</b>
                 </div>
                 <div>
-                     <p>Оптом (от 5 штук):</p>
-                    <b>{price2}</b></div>
-                <div>
-                     <p>В наличии:</p>
-                    <b>{price3}</b></div>
+                    <p>Old price:</p>
+                    <b>{old_price}</b>
+                </div>
             </div>
-           <div className="product-btn">
-            <div className="detail-value">
-                <button className="plus">+</button>
-                <span>95</span>
-                <button className="minus">-</button>
+            <div className='product-btn'>
+                <div className='detail-value'>
+                    <button className='plus'>+</button>
+                    <span>95</span>
+                    <button className='minus'>-</button>
+                </div>
+                <button className='btn'>Add to Cart</button>
             </div>
-           <button className="btn">{button}</button>
-           </div>
-
         </div>
-    )
-}
+    );
+};
---
+++ src/constants/data.js
@@ -0,0 +1,30 @@
+export const productData = [
+    {
+        id: 1,
+        img: "https://s3-alpha-sig.figma.com/img/d88b/b331/b43462781d4bb09a24dc856f18640163?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bOaIAJPSySREnLFvfRQJbX2d2m3PQOks8SJZjCJ4JZhWsHG6sQgX2I0oD3B5ulO5tIaHSN8tSIlHQgHmGYJhQCNXBS99wV9JwohcrckBu4Fld7tZcGvTcbZAIkEw~gBzJvqdf78t6XdiulSNm24lyJsMLa6TxNqyeEgKxBLFtx1EjStdrs4IqqZvcNK2DdEJ3E6mlomVReZG-Bj02pfhC8d2K3MIuP4LQIUU7XT7hu~cH79C6XTXFspHMn6uUITe4k53n5g8LQ0ybWrX~IDKbvxeuqaPqM9cjWeydgLr54dx8dHGWlrWLSB4R-ONBdXzZMeh2Tp52lSEbcRmNNUiag__",
+        title: "iPhone 7 Plus Шлейф кнопки включения + Шлейф громкости Оригинал",
+        price: 95000,
+        old_price: 120000,
+    },
+    {
+        id: 2,
+        img: "https://s3-alpha-sig.figma.com/img/d88b/b331/b43462781d4bb09a24dc856f18640163?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bOaIAJPSySREnLFvfRQJbX2d2m3PQOks8SJZjCJ4JZhWsHG6sQgX2I0oD3B5ulO5tIaHSN8tSIlHQgHmGYJhQCNXBS99wV9JwohcrckBu4Fld7tZcGvTcbZAIkEw~gBzJvqdf78t6XdiulSNm24lyJsMLa6TxNqyeEgKxBLFtx1EjStdrs4IqqZvcNK2DdEJ3E6mlomVReZG-Bj02pfhC8d2K3MIuP4LQIUU7XT7hu~cH79C6XTXFspHMn6uUITe4k53n5g8LQ0ybWrX~IDKbvxeuqaPqM9cjWeydgLr54dx8dHGWlrWLSB4R-ONBdXzZMeh2Tp52lSEbcRmNNUiag__",
+        title: "Samsung S22",
+        price: 100000,
+        old_price: 150000,
+    },
+    {
+        id: 3,
+        img: "https://s3-alpha-sig.figma.com/img/d88b/b331/b43462781d4bb09a24dc856f18640163?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bOaIAJPSySREnLFvfRQJbX2d2m3PQOks8SJZjCJ4JZhWsHG6sQgX2I0oD3B5ulO5tIaHSN8tSIlHQgHmGYJhQCNXBS99wV9JwohcrckBu4Fld7tZcGvTcbZAIkEw~gBzJvqdf78t6XdiulSNm24lyJsMLa6TxNqyeEgKxBLFtx1EjStdrs4IqqZvcNK2DdEJ3E6mlomVReZG-Bj02pfhC8d2K3MIuP4LQIUU7XT7hu~cH79C6XTXFspHMn6uUITe4k53n5g8LQ0ybWrX~IDKbvxeuqaPqM9cjWeydgLr54dx8dHGWlrWLSB4R-ONBdXzZMeh2Tp52lSEbcRmNNUiag__",
+        title: "Redme Note 12",
+        price: 55000,
+        old_price: 90000,
+    },
+    {
+        id: 4,
+        img: "https://s3-alpha-sig.figma.com/img/d88b/b331/b43462781d4bb09a24dc856f18640163?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bOaIAJPSySREnLFvfRQJbX2d2m3PQOks8SJZjCJ4JZhWsHG6sQgX2I0oD3B5ulO5tIaHSN8tSIlHQgHmGYJhQCNXBS99wV9JwohcrckBu4Fld7tZcGvTcbZAIkEw~gBzJvqdf78t6XdiulSNm24lyJsMLa6TxNqyeEgKxBLFtx1EjStdrs4IqqZvcNK2DdEJ3E6mlomVReZG-Bj02pfhC8d2K3MIuP4LQIUU7XT7hu~cH79C6XTXFspHMn6uUITe4k53n5g8LQ0ybWrX~IDKbvxeuqaPqM9cjWeydgLr54dx8dHGWlrWLSB4R-ONBdXzZMeh2Tp52lSEbcRmNNUiag__",
+        title: "Honor",
+        price: 95000,
+        old_price: 120000,
+    },
+];
