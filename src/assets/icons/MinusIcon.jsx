ad5ca289e14e0b0ac73667b23ed2ba3723292db0
nosirxon650 <nurmetovnosirxon668@gmail.com>
2025-01-09 14:18:59 +0500

    added: cart page

---
+++ src/assets/icons/MinusIcon.jsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+const MinusIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={12}
+    height={12}
+    style={{
+      fill: "#000",
+    }}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M5 11h14v2H5z" />
+  </svg>
+)
+export default MinusIcon
